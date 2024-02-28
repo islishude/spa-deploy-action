a4fc18dc11d501f77884e186191bdef69cdfef95
islishude <islishude@gmail.com>
2024-02-28 13:25:19 +0800

    fix: typo for cache-control-merge-policy input

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const prefix = core.getInput('s3-bucket-prefix')
 const dirPath = core.getInput('dir-path', { required: true })
 const isDelete = core.getBooleanInput('delete')
 const defaultCacheControl = core.getInput('default-cachec-control')
-const cacheControlMergePolicy = core.getInput('cachec-control-merge-policy')
+const cacheControlMergePolicy = core.getInput('cache-control-merge-policy')
 const cacheControlJson = JSON.parse(core.getInput('cache-control'))
 
 run({
