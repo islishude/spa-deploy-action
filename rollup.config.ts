02648f4147c889ce0eb7abd2b013132071f44eb7
Shude Li <islishude@gmail.com>
2026-02-24 10:36:42 +0800

    build: bump eslint to v10 (#210)
    * build: bump eslint to v10
    
    * Update dist
    
    * fix: update testMatch pattern in jest.config.js for better matching
    
    * fix: remove trailing comma in jest.config.js
    
    * fix: standardize port syntax and remove NODE_OPTIONS from CI configuration
    
    * fix: standardize port syntax in CI configuration
    
    * fix: add .github to .prettierignore
    
    * fix: standardize ports syntax in CI configuration

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,20 +1,25 @@
 // See: https://rollupjs.org/introduction/
 
 import commonjs from '@rollup/plugin-commonjs'
-import jsonPlugin from '@rollup/plugin-json'
+import json from '@rollup/plugin-json'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 
 const config = {
   input: 'src/index.ts',
   output: {
-    inlineDynamicImports: true,
     esModule: true,
     file: 'dist/index.js',
     format: 'es',
+    inlineDynamicImports: true,
     sourcemap: true
   },
-  plugins: [typescript(), nodeResolve(), commonjs(), jsonPlugin()]
+  plugins: [
+    typescript(),
+    nodeResolve({ preferBuiltins: true }),
+    json(),
+    commonjs()
+  ]
 }
 
 export default config
